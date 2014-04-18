02bc54a4bfb0c0a4eee6303b04b4a51e9d51abf7
Atanas Plachkov <nas.plachkov@gmail.com>
2014-04-18 17:01:14 +0300

    Some POSIX signal handling

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,18 @@
 
 #include "httpserver.h"
 
+#include <signal.h>
+
+void signalHandler(int sig)
+{
+    qDebug() << "Received: " << sig;
+
+    // TODO: "/disconnect" the store from the master server and probably shutdown the application
+
+    // TODO: Remove this
+    exit(0);
+}
+
 int main(int argc, char **argv)
 {
     QCoreApplication a(argc, argv);
@@ -12,6 +24,15 @@ int main(int argc, char **argv)
 
     qDebug() << "Starting up the server...\n";
     HttpServer piHttpServer(&settings);
+
+    // Handle some POSIX signals
+    struct sigaction sig;
+    sig.sa_flags = SA_SIGINFO;
+    sig.sa_handler = &signalHandler;
+
+    sigaction(SIGINT, &sig, NULL);
+    sigaction(SIGHUP, &sig, NULL);
+    sigaction(SIGTERM, &sig, NULL);
 
     return a.exec();
 }
